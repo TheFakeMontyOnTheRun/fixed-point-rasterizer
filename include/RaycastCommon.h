453bfddb6965729ae8e5c6e4de01d00242d3b91b
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2017-11-03 14:20:25 +0000

    Remove unused stuff

--- include/RaycastCommon.h
+++ include/RaycastCommon.h
@@ -6,15 +6,6 @@
 #define BLANKSLATE_COMMON_H
 
 namespace odb {
-    enum class ECommand {
-        kUp,
-        kRight,
-        kDown,
-        kLeft,
-        kFire1,
-        kFire2
-    };
-
     using FixP = fixed_point<int32_t, -16>;
 
     class Vec3 {
@@ -32,14 +23,5 @@ namespace odb {
         FixP mX;
         FixP mY;
     };
-
-    using CControlCallback = std::function< void(ECommand) >;
-
-    int wrap360(int i);
-
-    const int xRes = 255;
-    const int yRes = 128;
-    const FixP fixedYRes = FixP{128};
-
 }
 #endif //BLANKSLATE_COMMON_H
--- src/RaycastCommon.cpp
+++ src/RaycastCommon.cpp
@@ -20,16 +20,6 @@ using sg14::fixed_point;
 
 namespace odb {
 
-    int wrap360(int i) {
-        int angle = static_cast<int>( i ) % 360;
-
-        while (angle < 0) {
-            angle += 360;
-        }
-
-        return angle;
-    }
-
     Vec3 operator+(const Vec3& v1, const Vec3& v2 ) {
         return { v1.mX + v2.mX, v1.mY + v2.mY, v1.mZ + v2.mZ };
     }
